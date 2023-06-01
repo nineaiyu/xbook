5df7972e4a713e2816a8dce2b135bfd031688330
isummer <nineven@qq.com>
2023-06-01 11:09:43 +0800

    修复书籍图片访问失败问题

--- docker/init/clean.sh
+++ docker/init/clean.sh
@@ -2,7 +2,7 @@
 #
 #
 
-for i in nginx xbook mariadb redis xbook_client build-buildxbook-1;do echo $i;docker rm -f $i;done
+for i in xbook_nginx xbook xbook_mariadb xbook_redis xbook_client build-buildxbook-1;do echo $i;docker rm -f $i;done
 
 
 docker network rm xbook
